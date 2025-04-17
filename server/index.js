191183aa23085be2b0f772ce73445cc3ccca54ab
sushanthraiurwa <158015490+sushanthraiurwa@users.noreply.github.com>
2025-04-17 21:59:21 +0530

    Made updateds for deploy on the render

--- server/index.js
+++ server/index.js
@@ -160,7 +160,5 @@ app.post('/generate-pdf', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
+
